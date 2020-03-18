28f071bce43fa77d701c5846b35bc6a575c90a22
Nancy <nixiangyu@ofbank.com>
2020-03-18 17:20:52 +0800

    [ID1000375] implement the block chain proof picture components
    --story=1000375 --user=Nixiangyu
    https://www.tapd.cn/60210943/prong/stories/view/1160210943001000375
    
    Signed-off-by: Nixiangyu nixiangyu@ofworld.com

--- components/image/image.go
+++ components/image/image.go
@@ -7,7 +7,6 @@ SPDX-License-Identifier: Apache-2.0
 package image
 
 import (
-	"errors"
 	"image"
 	"image/color"
 	"image/draw"
@@ -17,10 +16,10 @@ import (
 
 	"github.com/csiabb/donation-service/common/log"
 	"github.com/csiabb/donation-service/common/rest"
+	"github.com/csiabb/donation-service/components/wx"
 
 	"github.com/golang/freetype"
 	"github.com/golang/freetype/truetype"
-	"github.com/skip2/go-qrcode"
 	"golang.org/x/image/math/fixed"
 )
 
@@ -83,23 +82,11 @@ func (c *Client) Init() error {
 	return nil
 }
 
-// CreateQrCode  create a new qr code
-func (c *Client) CreateQrCode(content string) (img image.Image, err error) {
-	var qrCode *qrcode.QRCode
-
-	qrCode, err = qrcode.New(content, qrcode.Highest)
-
-	if err != nil {
-		return nil, errors.New("qr code creation failed")
-	}
-
-	qrCode.DisableBorder = true
-	qrCode.BackgroundColor = rest.Color3
-	qrCode.ForegroundColor = rest.Color2
-
-	img = qrCode.Image(rest.QrCodeSize)
-
-	return img, nil
+// CreateQrCode  create a wx qr code
+func (c *Client) CreateWXQrCode(appID string, secret string, scene string) (img image.Image, err error) {
+	token, err := wx.GetAccessToken(appID, secret)
+	qrCode, err := wx.GetWXACode(token, scene)
+	return qrCode, err
 }
 
 // DrawText define string drawing
@@ -138,7 +125,7 @@ func (c *Client) SlipString(content string, fontSize float64, textWidth int) []s
 }
 
 // CreateDonationImage create new image of donation items
-func (c *Client) CreateDonationImage(content []string, isShare bool) (*image.NRGBA, error) {
+func (c *Client) CreateDonationImage(content []string, appID string, secret string, scene string, isShare bool) (*image.NRGBA, error) {
 	// init bg image
 	err := c.Init()
 	if err != nil {
@@ -161,8 +148,8 @@ func (c *Client) CreateDonationImage(content []string, isShare bool) (*image.NRG
 	// create qr image
 	if isShare {
 		var qrCodeImg image.Image
-		qrCodeImg, err = c.CreateQrCode("http://www.baidu.com")
-		draw.Draw(c.Bg, qrCodeImg.Bounds().Add(image.Pt(100, 710)), qrCodeImg, image.Point{X: 0, Y: 0}, draw.Over)
+		qrCodeImg, err = c.CreateWXQrCode(appID, secret, scene)
+		draw.Draw(c.Bg, qrCodeImg.Bounds().Add(image.Pt(100, 720)), qrCodeImg, image.Point{X: 0, Y: 0}, draw.Over)
 		err = c.DrawText(rest.Color2, rest.QRContent, freetype.Pt(235, 828), 22)
 	}
 
